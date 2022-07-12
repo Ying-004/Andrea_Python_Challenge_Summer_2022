c5049dae7aeac3c3f07a4ad58177897e5d774363
Ying-004 <1184425253@qq.com>
2022-07-12 19:35:03 +0800

    上课ing

--- Chapter 01/ComputerExpression.py
+++ Chapter 01/ComputerExpression.py
@@ -1,7 +1,6 @@
 # Compute expression
 print((10.5+2*3)/(45-3.5))
-print("3.5*4"/2-2.5")
-print(3.5*4/2-2.5)
+
 
 
 
--- Chapter 01/turtle.py
+++ Chapter 01/turtle.py
@@ -1,6 +1,12 @@
-import turtle
-# Import turtle module
+from turtle import *
+turtle.hideturtle()
 turtle.showturtle()
 turtle.write("welcome to python")
 turtle.forward(100)
+turtle.right(90)
+turtle.color("green")
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(80)
+
 
