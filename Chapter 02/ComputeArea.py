c11cc60b7a4077be07b03fcd84c59273ac72f589
Ying-004 <1184425253@qq.com>
2022-07-17 21:18:06 +0800

    修改文件顺序

--- Chapter 02/ComputeArea.py
+++
@@ -1,8 +0,0 @@
-#Assign a value to radius
-radius = 20 #radius is now 20
-
-# Comute aarea
-area = radius * radius * 3.14159
-
-#Display results
-print("The area for the circle of radius", radius, "is",area)
--- Chapter 02/定名常量.py
+++
@@ -1,8 +0,0 @@
-#Assign a radius
-radius = 20 # radius is now 20
-
-#Compute area
-PI= 3.14159
-area = radius * radius * PI
-#Display results
-print("The area for the circle of radius", radius, "is", area)
