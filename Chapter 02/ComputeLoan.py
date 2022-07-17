ef854e4ade4c3ae659255eb32bfa99dba6fc3d4d
Ying-004 <1184425253@qq.com>
2022-07-17 21:29:09 +0800

    完成进度到： # Enter annual interest rate as a percentage, e.g.,7.25 annualInterestRate = eval(input(         "Enter annual interest rate, e.g., 7.25"))
    monthlyInterestRate = annualInterestRate / 1200
    
    # Enter Number of years
    numberOfYears = eval(input(
            "Enter number of years as an integer, e.g., 5:"))
    
    # Enter loan amount
    loanAmount = eval(input("Enter loan amount, e.g.,120000.95:"))

---
+++ Chapter 02/ComputeLoan.py
@@ -0,0 +1,13 @@
+# Enter annual interest rate as a percentage, e.g.,7.25
+annualInterestRate = eval(input(
+        "Enter annual interest rate, e.g., 7.25"))
+
+monthlyInterestRate = annualInterestRate / 1200
+
+# Enter Number of years
+numberOfYears = eval(input(
+        "Enter number of years as an integer, e.g., 5:"))
+
+# Enter loan amount
+loanAmount = eval(input("Enter loan amount, e.g.,120000.95:"))
+
