9cc647d3cfcb34fbd725cb3d90321e0dcc2a0031
Ying-004 <1184425253@qq.com>
2022-07-22 21:39:11 +0800

    进度到： import turtle
    # Prompt the user for inputting two points
    x1, y1 = eval(input("Enter x1 and y1 for point 1:"))
    x2, y2 = eval(input("Enter x2 and y2 for point 2:"))
    
    #Compute the distance
    distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
    
    # Display two points and the connecting line
    turtle.penup()
    turtle.goto((x1 + x2) / 2, (y1 + y2) / 2)
    turtle.write(distance)
    
    turtle.done()

--- Chapter 02/ComputeDistanceGraphics.py
+++ Chapter 02/ComputeDistanceGraphics.py
@@ -1,4 +1,4 @@
-Import turtle
+import turtle
 
 # Prompt the user for inputting two points
 x1, y1 = eval(input("Enter x1 and y1 for point 1:"))
@@ -6,4 +6,10 @@ x2, y2 = eval(input("Enter x2 and y2 for point 2:"))
 
 #Compute the distance
 distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-v
+
+# Display two points and the connecting line
+turtle.penup()
+turtle.goto((x1 + x2) / 2, (y1 + y2) / 2)
+turtle.write(distance)
+
+turtle.done()
