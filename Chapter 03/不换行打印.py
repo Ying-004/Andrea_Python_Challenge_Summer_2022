0577901293461983fa45908382d5c6a271a9f62d
Ying-004 <1184425253@qq.com>
2022-07-22 22:31:06 +0800

    运行了： #不换行打印例子1 print("AAA", end = " ") print("BBB", end = "") print("BBB", end = "***") print("BBB", end = "***") print("") #不换行打印例子2 import math radius = 3 print("The area is", radius* radius * math.pi, end = ' ') print("and the perimeter is",2 * radius * math.pi)

---
+++ Chapter 03/不换行打印.py
@@ -0,0 +1,11 @@
+#不换行打印例子1
+print("AAA", end = " ")
+print("BBB", end = "")
+print("BBB", end = "***")
+print("BBB", end = "***")
+print("")
+#不换行打印例子2
+import math
+radius = 3
+print("The area is", radius* radius * math.pi, end = ' ')
+print("and the perimeter is",2 * radius * math.pi)
