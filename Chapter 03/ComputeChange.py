598334d232db9b5a3331af2210960ab61e2be247
Ying-004 <1184425253@qq.com>
2022-07-23 22:23:22 +0800

    use least coins to represent a certain amount of money

--- Chapter 03/ComputeChange.py
+++ Chapter 03/ComputeChange.py
@@ -9,4 +9,26 @@ numberOfOneDollars = remainingAmount // 100
 remainingAmount= remainingAmount % 100
 
 # Find the number of quarters
-numberOf
+numberOfQuarters = remainingAmount // 25
+remainingAmount = remainingAmount % 25
+
+# Find the number of dimes in the remaining amount
+numberOfDimes = remainingAmount // 10
+remainingAmount = remainingAmount % 10
+
+# Find the number of nickels in the remaining amount
+numberOfNickels = remainingAmount // 5
+remainingAmount = remainingAmount % 5
+
+#Find the number of penies in the remaining amount
+numberOfPennies = remainingAmount
+
+#Display the results
+print("Your amount",amount ,"consists of\n",
+    "\t", numberOfOneDollars, "dollars\n",
+    "\t", numberOfQuarters, "quarters\n",
+    "\t", numberOfDimes, "dimes\n",
+    "\t", numberOfNickels, "nickels\n",
+    "\t", numberOfPennies, "pennies\n",)
+
+
