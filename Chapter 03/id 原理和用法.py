da7f3f02a97797f83bcac484bf529ae2801f599c
Ying-004 <1184425253@qq.com>
2022-07-24 11:15:36 +0800

    不知道为什么这个程序无法运行，求解答thanks

---
+++ Chapter 03/id 原理和用法.py
@@ -0,0 +1,15 @@
+n = 3 # n is an integer
+id(n)
+505408904
+type(n)
+<class 'int'>
+f = 3.0 # f is a float
+id(f)
+26647120
+type(f)
+<class 'int'>
+s = "Welcome" # s is a string
+id(s)
+36201472
+tyoe(s)
+<class 'str'>
