76d597f7e773e2262293344b214488c993cb2b19
Andrea Zhang <yogurt.528@icloud.com>
2022-08-13 09:45:57 +0800

    a simple GUI window is displayed in this code

---
+++ Chapter 09/SimpleGUI.py
@@ -0,0 +1,8 @@
+from tkinter import * # Import all definitions from tkinter
+
+window = Tk() # Create a window
+label = Label(window, text = "Welcome to Python") # Create a label
+button =  Button(window, text = "Click Me") # create a button
+label.pack() # Place the label in the window
+button.pack() # Place the button in the window
+window.mainloop() # Create an event loop
