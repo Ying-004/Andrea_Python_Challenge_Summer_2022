687b9a3c2c39c5e1837daa62297b81faff90ced8
Andrea Zhang <yogurt.528@icloud.com>
2022-08-19 07:22:25 +0800

    Eventually everything goes right after delete the extra square bracket

--- Chapter 11/NearestPoints.py
+++ Chapter 11/NearestPoints.py
@@ -12,7 +12,7 @@ def nearestPoints(points):
     # Compute distance between every two points
     for i in range(len(points)):
         for j in range(i + 1, len(points)):
-            d = distance(points[i][0], points[i][1],points[[j][0], points[j][1]) # Find distance
+            d = distance(points[i][0], points[i][1],points[j][0], points[j][1]) # Find distance
 
             if shortestDistance > d:
                 p1, p2 = i , j # update p1, p2
