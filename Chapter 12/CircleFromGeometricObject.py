b3ce3861432c8b06aff841339f4cefb68b3af01d
Andrea Zhang <yogurt.528@icloud.com>
2022-08-22 10:43:09 +0800

    Circle is derived from the object GeometricObject

---
+++ Chapter 12/CircleFromGeometricObject.py
@@ -0,0 +1,25 @@
+from GeometricObject import GeometricObject
+import math  # math.pi is used in the class
+
+class Circle(GeometricObject):
+    def __init__(self, radius):
+        super().__init__()
+        self.__radius = radius
+
+    def getRadius(self):
+        return self.__radius
+
+    def setRadius(self, radius):
+        self.__radius = radius
+
+    def getArea(self):
+         return self.__radius * self.__raidus * math.pi
+
+    def getDiameter(self):
+        return 2 * self.__radius
+
+    def getPerimeter(self):
+        return 2 * self.__radius * math.pi
+
+    def printCircle(self):
+        print(self.__str__() + " radius: " + str(self.__radius))
