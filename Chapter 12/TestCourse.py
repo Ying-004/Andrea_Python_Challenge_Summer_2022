4e634415fc1be77478fed79fee6d617cc20f1489
Andrea Zhang <yogurt.528@icloud.com>
2022-08-24 13:50:55 +0800

    design a course class to add new students

---
+++ Chapter 12/TestCourse.py
@@ -0,0 +1,22 @@
+from Course import Course
+
+def main():
+    course1 = Course("Data Structures")
+    course2 =Course("Database Systems")
+
+    course1.addStudent("Peter Jones")
+    course1.addStudent("Brain Smith")
+    course1.addStudent("Anne Kennedy")
+
+    course2.addStudent("Peter Jones")
+    course2.addStudent("Steve Smith")
+
+    print("Number of students in course1:",
+          course2.getNumberOfStudents())
+    students = course1.getStudents()
+    for student in students:
+        print(student, end = ", ")
+
+    print("\nNumber of students in course2:",
+          course2.getNumberOfStudents())
+main()  # Create the main function
